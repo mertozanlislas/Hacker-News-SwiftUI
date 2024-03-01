7bbcc89d6c41c0431496de8fbf6695ac20d81036
mertozanlislas <ozanlislas66@gmail.com>
2024-03-02 00:10:20 +0300

    Initial Commit

---
+++ Hacker-News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker-News
+//
+//  Created by Mert Ozan Lislas on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
